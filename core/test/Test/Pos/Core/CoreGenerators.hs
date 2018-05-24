ab29308d7c00fa41b04d71f891aa1a65ddfb0802
Jordan Millar <jordan.millar@iohk.io>
2018-05-24 15:21:45 -0400

    [CDEC-311] Add golden tests for remaining TxInWitness constructors

--- core/test/Test/Pos/Core/CoreGenerators.hs
+++ core/test/Test/Pos/Core/CoreGenerators.hs
@@ -7,26 +7,28 @@ import           Data.Maybe (fromJust)
 import           Data.Text
 import           Hedgehog (Gen)
 import           Pos.Binary.Core()
-import           Pos.Core.Common ( Address (..), Coin (..), IsBootstrapEraAddr (..)
+import           Pos.Core.Common (Address (..), Coin (..), IsBootstrapEraAddr (..)
                                  , makePubKeyAddress, Script (..))
 import           Pos.Core.Txp ( Tx (..), TxIn (..), TxInWitness (..)
                               , TxOut (..), TxSig , TxSigData (..))
 import           Pos.Crypto.Configuration
 import           Pos.Crypto.Hashing (unsafeHash)
-import           Pos.Crypto.Signing ( createKeypairFromSeed, parseFullPublicKey, PublicKey(..)
+import           Pos.Crypto.Signing (createKeypairFromSeed, parseFullPublicKey, PublicKey(..)
                                     , redeemDeterministicKeyGen, redeemPkBuild, RedeemPublicKey(..)
                                     , RedeemSecretKey(..), redeemSign, RedeemSignature(..), sign
                                     , SecretKey(..), Signature (..), SignTag(..))
 import           Pos.Data.Attributes (mkAttributes)
 import           Prelude
---import qualified Test.Pos.Util.Base16 as B16
 
+import qualified Crypto.Sign.Ed25519 as Ed25519
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
+
+
 --------------- Hedgehog Core Generators ---------------
 
--- | Generates `PublicKey` with no password. TODO: Double check this
+-- | Generates `PublicKey` with  empty password.
 
 genPubKey :: Gen PublicKey
 genPubKey = do
@@ -97,7 +99,7 @@ genTxSig = do
     signTag <- genSignTag
     sk <- genSecKey
     txSigData <- genTxSigData
-    return $ (sign (ProtocolMagic pM) signTag sk txSigData)
+    return $ sign (ProtocolMagic pM) signTag sk txSigData
 
 genPkWit :: Gen TxInWitness
 genPkWit = do
@@ -108,7 +110,7 @@ genPkWit = do
 genScript :: Gen Script
 genScript = do
     version <- Gen.word16 Range.constantBounded
-    sScript <- Gen.bytes (Range.singleton 32)     -- arbitrarily picked script serialization length
+    sScript <- Gen.bytes (Range.singleton 32)
     return $ Script version sScript
 
 genScriptWit :: Gen TxInWitness
@@ -126,8 +128,6 @@ genRedSecKey :: Gen RedeemSecretKey
 genRedSecKey = do
     bytes <- Gen.bytes (Range.singleton 32)
     return . snd $ fromJust (redeemDeterministicKeyGen bytes)
-
-
 
 genRedSig :: Gen (RedeemSignature TxSigData)
 genRedSig = do
@@ -149,21 +149,21 @@ genUnkWit = do
     bs <- Gen.bytes (Range.constant 0 50)
     return $ UnknownWitnessType word bs
 
-------------------------------------------------------------------------
 
--- Golden test data
+------------------------- Golden Test Data -----------------------------
 
+-- PkWitness
 
 seedBS :: ByteString
 seedBS = "nvpbkjoxpmemkgicmqxdixdwrsbhwdtqxhtzipzfwkjzzkwifcgszbqmfjyrxrrg"
 
 -- | `XSignature` constructors are not exported from cardano-crypto.
 -- Therefore `TxSig` must be constructed with `xsignature`.
+
 txSig :: TxSig
 txSig = case xsignature seedBS of
             Right xsig -> Signature xsig
             Left err -> Prelude.error $ "txSig error:" ++ err
-
 
 -- | `seedPubK` generated with `formatFullPublicKey` and `genPubKey`.
 
@@ -171,12 +171,34 @@ seedPubK :: Text
 seedPubK = "eFZDwMJmAX3KKaRV/zLx2PabIE3un79kQ36oZuKFUMjA6wW5zsmOmvRwWoL8gMfw\
            \+xNz1bSHAZvRt47V+iTb7g=="
 
-
 pubKey :: PublicKey
-pubKey = case (parseFullPublicKey seedPubK) of
+pubKey = case parseFullPublicKey seedPubK of
             Right pk -> pk
             Left err -> Universum.error (toText ("Seed public key failed to parse: " :: Text) `Data.Text.append` err)
 
 pkWitness :: TxInWitness
 pkWitness = PkWitness pubKey txSig
 
+-- ScriptWitness
+
+scrWitness :: TxInWitness
+scrWitness = ScriptWitness (Script 52147 "ootteiijvizitypuleyshqtoihcncwia") (Script 732 "xkuiffugzccekevklqdcvjrkrwrxjwdg")
+
+-- RedeemWitness
+
+redWitPubKey :: RedeemPublicKey
+redWitPubKey = RedeemPublicKey $ Ed25519.PublicKey "cnrzviaomratmiowjsrqpjmhlfwnlrnu"
+
+redWitSig :: RedeemSignature TxSigData
+redWitSig = RedeemSignature (Ed25519.Signature "34r\GSg\129\DC3\209\228\245]\170\189\158\161\22 \
+                                                \ 9\t\150'\249\EM$\226q\225\DC1?\136\194\171\15 \
+                                                \ 7\167\136t\248\ff\167\203\222\224\142=?\DC34$ \
+                                                \z\228Ev\250\161:\a\148\SO\175\233\239\150\t\CAN\ETX")
+
+redWitness :: TxInWitness
+redWitness =  RedeemWitness redWitPubKey redWitSig
+
+-- UnknownTypeWitness
+
+unkWitness :: TxInWitness
+unkWitness = UnknownWitnessType 127 "\133\SYNE\STX"
--- core/test/Test/Pos/Core/TxInWitnessProp.hs
+++ core/test/Test/Pos/Core/TxInWitnessProp.hs
@@ -4,12 +4,14 @@ module Test.Pos.Core.TxInWitnessProp
 
 import           Universum
 
-import           Hedgehog ((===), checkParallel, discover, forAll, property, Property, withTests)
+import           Hedgehog ( (===), checkParallel, discover, forAll, property
+                          , Property, withTests)
 import           Pos.Binary.Class (serialize)
 import           Test.Pos.Core.Tripping (trippingBiShow)
-import           Test.Pos.Core.CoreGenerators (genPkWit, genRedWit, genScriptWit, genUnkWit, pkWitness)
+import           Test.Pos.Core.CoreGenerators ( genPkWit, genRedWit, genScriptWit, genUnkWit
+                                              , pkWitness, scrWitness, redWitness, unkWitness)
 
-
+-- import qualified Test.Pos.Util.Base16 as B16
 --import qualified Hedgehog as H
 import qualified Test.Pos.Util.Base16 as B16
 
@@ -17,8 +19,8 @@ import qualified Test.Pos.Util.Base16 as B16
 
 ------------------------------------------------------------------------
 
-prop_golden_TestPkwitness :: Property
-prop_golden_TestPkwitness =
+prop_golden_TestPkWitness :: Property
+prop_golden_TestPkWitness =
     withTests 1 . property $
         B16.encodeWithIndex (serialize pkWitness) ===  "00: 8200d8185885825840785643c0c26601\n\
                                                        \10: 7dca29a455ff32f1d8f69b204dee9fbf\n\
@@ -29,6 +31,34 @@ prop_golden_TestPkwitness =
                                                        \60: 78647772736268776474717868747a69\n\
                                                        \70: 707a66776b6a7a7a6b7769666367737a\n\
                                                        \80: 62716d666a797278727267\n"
+prop_golden_TestScrWitness :: Property
+prop_golden_TestScrWitness =
+    withTests 1 . property $
+        B16.encodeWithIndex (serialize scrWitness) ===  "00: 8201d818584d828219cbb358206f6f74\n\
+                                                        \10: 746569696a76697a69747970756c6579\n\
+                                                        \20: 736871746f6968636e63776961821902\n\
+                                                        \30: dc5820786b7569666675677a6363656b\n\
+                                                        \40: 65766b6c716463766a726b727772786a\n\
+                                                        \50: 776467\n"
+
+prop_golden_TestRedWitness :: Property
+prop_golden_TestRedWitness =
+    withTests 1 . property $
+        B16.encodeWithIndex (serialize redWitness) === "00: 8202d818586c825820636e727a766961\n\
+                                                       \10: 6f6d7261746d696f776a737271706a6d\n\
+                                                       \20: 686c66776e6c726e7558473334721d67\n\
+                                                       \30: 8113d1e4f55daabd9ea1162020390996\n\
+                                                       \40: 27f91924e271e1113f88c2ab0f202037\n\
+                                                       \50: a78874f80c66a7cbdee08e3d3f133424\n\
+                                                       \60: 207ae44576faa13a07940eafe9ef9609\n\
+                                                       \70: 1803\n"
+
+
+prop_golden_TestUnkWitness :: Property
+prop_golden_TestUnkWitness =
+    withTests 1 . property $
+        B16.encodeWithIndex (serialize unkWitness) === "82187fd8184485164502"
+
 
 
 -- | Round trip tests
